05dd7bb8cb365e404a1bb14d6ba56e0f85ddde6d
Joseph Clyde Mar <clydemar234@gmail.com>
2023-10-17 15:01:56 +0800

    Initial commit

---
+++ public/js/script.js
@@ -0,0 +1,30 @@
+ 
+const socket = io('ws://');
+
+
+const sendMessage = (event) => {
+    event.preventDefault();
+    const button = document.getElementById('messageButton');
+    const input = document.getElementById('textField');
+    if (input.value) {
+        // console.log(input.value);
+        socket.emit('message', input.value);
+        input.value = '';
+    }
+    // socket.send('INITIATE');
+    input.focus();
+};
+
+
+// Listen for messages
+socket.on('message', (data) => {
+    const li = document.createElement('li');
+    li.textContent = data;
+    document.getElementById('messageBox').appendChild(li);
+});
+
+
+
+document.getElementById('messageForm').addEventListener('submit', sendMessage);
+
+
---
+++ server.js
@@ -0,0 +1,63 @@
+// import { createServer } from 'http';
+import express from 'express';
+import { Server } from 'socket.io';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import cors from 'cors';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+
+
+const PORT = process.env.PORT || 8900;
+
+// const httpServer = createServer();
+const app = express();
+
+app.use(cors());
+app.use(express.static(path.join(__dirname, 'public')));
+
+const expressServer = app.listen(PORT, () => {
+    console.log(`Listening on PORT: ${PORT}`);
+});
+
+
+
+
+// const io = new Server(httpServer, {
+//     cors: {
+//         origin: process.env.NODE_ENV === 'production' ? false : [
+//             'http://127.0.0.1:5500',
+//             'http://localhost:5500'
+//         ]
+//     }
+// });
+
+const io = new Server(expressServer, {
+    cors: {
+        origin: '*'
+    }
+});
+
+
+
+io.on('connection', (socket) => {
+    console.log(`CONNECTED -> ${socket.id}`);
+    socket.on('message', (data) => {
+        io.emit('message', `${socket.id}: ${data}`);
+        console.log(`${socket.id}:${data}`);
+    });
+    socket.on('disconnect', () => {
+        console.log(`DISCONNECTED -> ${socket.id}`);
+    });
+});
+
+
+
+
+
+// httpServer.listen(PORT, () => {
+//     console.log(`Listening on ${PORT}`);
+// });
+
